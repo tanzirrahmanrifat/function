8dbe95d08ec5a5e49a8bc88f0fe3d064158c4039
Tanzir Rifat <93050315+tanzirrahmanrifat@users.noreply.github.com>
2022-09-12 22:45:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Function tech 
+
+// Name function 
+function myName(){
+    console.log(Date.now());
+}
+myName()
+
+
+// variable function 
+let car = function(){
+    console.log('test');
+}
+car()
+
+// arrow function 
+let myCar = ()=>{
+    console.log(Date.now());
+}
+myCar()
